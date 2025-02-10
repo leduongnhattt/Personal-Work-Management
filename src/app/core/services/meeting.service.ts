7eac63bdc14c85307cfa1bc4d8bc162f80cc9f23
leduongnhattt <nhatnhatnhatle@gmail.com>
2025-02-10 16:01:21 +0700

    build UI setting and finish features light/night mode

--- src/app/core/services/meeting.service.ts
+++ src/app/core/services/meeting.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { environment } from '../../../environments/environment.development';
 
 @Injectable({
@@ -11,15 +11,31 @@ export class MeetingService {
   constructor(private http: HttpClient) { }
 
   createMeeting(meeting: any): Observable<any>{
-    return this.http.post(environment.apiMeetingUrl + '/addApointment', meeting);
+    return this.http.post(environment.apiMeetingUrl + '/addApointment', meeting)
+    .pipe(
+      catchError(error => this.handleError(error))
+    );
   }
   getAllMeetings(): Observable<any>{
-    return this.http.get(environment.apiMeetingUrl + '/allApointment');
+    return this.http.get(environment.apiMeetingUrl + '/allApointment')
+    .pipe(
+      catchError(error => this.handleError(error))
+    );
   }
   updateMeeting(meetingId: string, meeting: any): Observable<any>{
-    return this.http.put(environment.apiMeetingUrl + '/updateApointment/' + meetingId, meeting);
+    return this.http.put(environment.apiMeetingUrl + '/updateApointment/' + meetingId, meeting)
+    .pipe(
+      catchError(error => this.handleError(error))
+    );
   }
   deleteMeeting(meetingId: string): Observable<any>{
-    return this.http.delete(environment.apiMeetingUrl + '/deleteApointment/' + meetingId);
+    return this.http.delete(environment.apiMeetingUrl + '/deleteApointment/' + meetingId)
+    .pipe(
+      catchError(error => this.handleError(error))
+    );
+  }
+  private handleError(error: any): Observable<never> {
+    console.error('API Error:', error);
+    return throwError(() => new Error(error.message || 'Something went wrong!'));
   }
 }
--- src/app/features/settings/settings.component.ts
+++ src/app/features/settings/settings.component.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder,  FormsModule,  ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-settings',
-    imports: [],
+    imports: [CommonModule, ReactiveFormsModule, FormsModule],
     templateUrl: './settings.component.html',
     styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
+  isDarkMode = false;
 
+  ngOnInit() {
+      const savedMode = localStorage.getItem('darkMode');
+      this.isDarkMode = savedMode === 'true';
+      this.applyDarkMode();
+  }
+
+  toggleDarkMode(event: any) {
+    this.isDarkMode = event.target.checked;
+    localStorage.setItem('darkMode', String(this.isDarkMode));
+    this.applyDarkMode();
+  }
+
+  applyDarkMode() {
+      document.body.classList.toggle('bg-dark', this.isDarkMode);
+      document.body.classList.toggle('text-white', this.isDarkMode);
+  }
 }
